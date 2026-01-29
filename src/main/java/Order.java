575237935d9cbea6ab75b9bbfab5dbee53540acf
Luicencia <lui100cia@gmail.com>
2026-01-29 15:57:38 +0300

    update

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -3,41 +3,29 @@ import java.util.List;
 import java.util.Objects;
 
 public class Order {
+    public enum PaymentStatus { UNPAID, PAID }
+
     private Integer id;
     private String reference;
     private Instant creationDatetime;
     private List<DishOrder> dishOrderList;
+    private PaymentStatus paymentStatus = PaymentStatus.UNPAID;
 
-    public Integer getId() {
-        return id;
-    }
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+    public String getReference() { return reference; }
+    public void setReference(String reference) { this.reference = reference; }
 
-    public String getReference() {
-        return reference;
-    }
+    public Instant getCreationDatetime() { return creationDatetime; }
+    public void setCreationDatetime(Instant creationDatetime) { this.creationDatetime = creationDatetime; }
 
-    public void setReference(String reference) {
-        this.reference = reference;
-    }
+    public List<DishOrder> getDishOrderList() { return dishOrderList; }
+    public void setDishOrderList(List<DishOrder> dishOrderList) { this.dishOrderList = dishOrderList; }
 
-    public Instant getCreationDatetime() {
-        return creationDatetime;
-    }
-
-    public void setCreationDatetime(Instant creationDatetime) {
-        this.creationDatetime = creationDatetime;
-    }
-
-    public List<DishOrder> getDishOrderList() {
-        return dishOrderList;
-    }
-
-    public void setDishOrderList(List<DishOrder> dishOrderList) {
-        this.dishOrderList = dishOrderList;
+    public PaymentStatus getPaymentStatus() { return paymentStatus; }
+    public void setPaymentStatus(PaymentStatus paymentStatus) {
+        this.paymentStatus = Objects.requireNonNull(paymentStatus);
     }
 
     @Override
@@ -46,6 +34,7 @@ public class Order {
                 "id=" + id +
                 ", reference='" + reference + '\'' +
                 ", creationDatetime=" + creationDatetime +
+                ", paymentStatus=" + paymentStatus +
                 ", dishOrderList=" + dishOrderList +
                 '}';
     }
@@ -58,15 +47,18 @@ public class Order {
         throw new RuntimeException("Not implemented");
     }
 
-
     @Override
     public boolean equals(Object o) {
         if (!(o instanceof Order order)) return false;
-        return Objects.equals(id, order.id) && Objects.equals(reference, order.reference) && Objects.equals(creationDatetime, order.creationDatetime) && Objects.equals(dishOrderList, order.dishOrderList);
+        return Objects.equals(id, order.id)
+                && Objects.equals(reference, order.reference)
+                && Objects.equals(creationDatetime, order.creationDatetime)
+                && Objects.equals(dishOrderList, order.dishOrderList)
+                && Objects.equals(paymentStatus, order.paymentStatus);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, reference, creationDatetime, dishOrderList);
+        return Objects.hash(id, reference, creationDatetime, dishOrderList, paymentStatus);
     }
 }
